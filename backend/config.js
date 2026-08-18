e434cf388d359954df079a635bd6961930f62633
dianaloz <dlbogota2104@gmail.com>
2026-08-18 13:25:05 -0400

    Changed data base to Neon

--- backend/config.js
+++ backend/config.js
@@ -10,11 +10,11 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
-// function getDatabaseUri() {
-//   return (process.env.NODE_ENV === "test")
-//       ? "jobly_test"
-//       : process.env.DATABASE_URL || "jobly";
-// }
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+    ? "jobly_test"
+    : process.env.DATABASE_URL || "jobly";
+}
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
@@ -31,5 +31,5 @@ console.log("---");
 module.exports = {
   SECRET_KEY,
   PORT,
-  BCRYPT_WORK_FACTOR
+  BCRYPT_WORK_FACTOR, getDatabaseUri
 };
--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,19 @@
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
-const password = require('./password')
-// const { getDatabaseUri } = require("./config");
+const { getDatabaseUri } = require("./config");
 
 let database;
-if (process.env.NODE_ENV === "test") {
-  database = "jobly_test";
-} else {
-  database = "jobly";
-}
-let db;
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    ssl: {
-      rejectUnauthorized: false
-    }, user: 'dianaloz',
-    host: '/var/run/postgresql',
-    database: database,
-    password: password,
-    port: 5432
-  });
-} else {
-  db = new Client({
-    user: 'dianaloz',
-    host: '/var/run/postgresql',
-    database: database,
-    password: password,
-    port: 5432
-  });
-}
+// if (process.env.NODE_ENV === "test") {
+//   database = "jobly_test";
+// } else {
+//   database = "jobly";
+// }
+
+let db = new Client({
+  connectionString: getDatabaseUri(),
+  ssl: { rejectUnauthorized: false }
+});
 
 db.connect();
 
